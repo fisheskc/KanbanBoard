b2639afc6a7e102044ae866651186331a04f59fe
TheOriginalCreator <wisdomdancelove@gmail.com>
2018-08-28 15:29:04 +0100

    Added card div

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let tasks = ['Task 1', 'Task 2', 'Task 3']
 
+let removeFunction = () => {
+    console.log(tasks)
+}
+
+document.getElementById('card').addEventListener('click', myFunction({
+    removeFunction()
+}));
