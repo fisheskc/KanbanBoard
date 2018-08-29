2ef54c660293c345cd8c807b5deabac967ae198b
TheOriginalCreator <wisdomdancelove@gmail.com>
2018-08-29 14:34:55 +0100

    Completed add function, delete function needs work though

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-let taskList = ['Task 1', 'Task 2']
+const addButton = document.getElementById('addCard')
+const input = document.getElementById('input')
+const list = document.getElementById('list')
 
-let addCard = () => {
-    let userInput = document.getElementById('toDoInput').value
-    console.log(userInput)
+let newTask;
+let remove;
+
+let taskList = []
+
+let addFunction = (i) => {
+    let userInput = input.value
+
+    remove = document.createElement('button')
+    newTask = document.createElement('li')
+
+    remove.innerHTML = "🗑️"
+
+    newTask.textContent = userInput
+
+    newTask.appendChild(remove)
+    list.appendChild(newTask)
+
+    taskList.push(newTask)
+
+    for (i = 0; i < taskList.length; i++) {
+        let taskValue = taskList[i].innerHTML;
+        taskList[i].id = i;
+        console.log(taskList)
+
+        trash = taskList[i].children[0]
+        trash.id = i
+    }
 }
 
-document.getElementById('addToDoCard').addEventListener('click', function(){
-    addCard()
+let removeFunction = () => {
+    console.log("It works!")
+}
+
+addButton.addEventListener("click", function () {
+    addFunction();
+})
+
+document.addEventListener('click', function (e, i) {
+    if (e.target.id == `${i}`) {
+        removeFunction()
+    }
 })
